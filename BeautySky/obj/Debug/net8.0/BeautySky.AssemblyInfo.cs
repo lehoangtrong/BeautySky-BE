d8e5e190d23b7a8ae87b0f159ac65d19bebb3dbb
Hoàng Hải <haile170504@gmail.com>
2025-03-13 13:42:18 +0700

    Edit again saveCarePlan

--- BeautySky/obj/Debug/net8.0/BeautySky.AssemblyInfo.cs
+++ BeautySky/obj/Debug/net8.0/BeautySky.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BeautySky")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b991b06d1519ecf56dc653ada505dbd3cf366a76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f197f3f26de957992bac7447c77637014456ee26")]
 [assembly: System.Reflection.AssemblyProductAttribute("BeautySky")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BeautySky")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
