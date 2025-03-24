2ad2e7e57b9ed168cd9f87acfd8eaac7c46cf4b2
danh181 <danhthanh18102004@gmail.com>
2025-03-24 21:29:54 +0700

    Edit Get method ProductsController

--- BeautySky/obj/Debug/net8.0/BeautySky.AssemblyInfo.cs
+++ BeautySky/obj/Debug/net8.0/BeautySky.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BeautySky")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9990a0a1241bcc1c544e3764d534a2ff20e9938f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f0d1f2f3e478ee2b570e780954ef5343ff118b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BeautySky")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BeautySky")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
