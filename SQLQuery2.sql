4b164834e3e164b0275f0c44c55d1f686b78d67f
huynhtoan3152004 <toanhhqe180078@fpt.edu.vn>
2025-03-25 13:54:34 +0700

    Add readme, Delete SQL file

--- SQLQuery2.sql
+++
@@ -1,4 +0,0 @@
-﻿INSERT INTO CarePlanStep (CarePlanId, StepOrder, StepName, StepDescription)
-VALUES 
-(3, 1, 'Làm sạch', 'Sử dụng sữa rửa mặt có chứa salicylic acid.'),
-(3, 2, 'Dưỡng ẩm', 'Sử dụng kem dưỡng ẩm không chứa dầu.');
