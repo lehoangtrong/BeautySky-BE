2b2813ddb282381ade0168727da964f991f5a8ca
danh181 <danhthanh18102004@gmail.com>
2025-03-27 20:05:31 +0700

    Fix bug CarePlanRoutineController

--- BeautySky/obj/Debug/net8.0/BeautySky.AssemblyInfo.cs
+++ BeautySky/obj/Debug/net8.0/BeautySky.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BeautySky")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89e65ecd7b84d07bb2ae7f86efb059c3fcadfb05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed50a8d19e0d41e441947f1934654680148d13b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BeautySky")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BeautySky")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
